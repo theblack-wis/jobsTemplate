2bc2f03d167ca7ded25551ee10ec84c10962305b
theblack-wis <70611244+theblack-wis@users.noreply.github.com>
2022-06-23 02:32:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -247,10 +247,10 @@
                                 type: 'POST',
                                 data: {data:""},
                                 success: function (dat) {
-                                    alert(dat.length );
+                                    alert(dat.length);
                                     if(dat.length > 0){
                                         console.log(dat);
-//                                         window.open(JSON.parse(dat)[0].offlink).focus();
+                                        // window.open(JSON.parse(dat)[0].offlink).focus();
                                     }else{
                                         window.open(deflink).focus();
                                     }
